3cef1fdeec5cab34324ccc84100c71ac446cd06f
jeonga96 <wazzang96jeonga@gmail.com>
2023-01-16 14:15:05 +0900

    [Feat] react-router-dom setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+/*  */
+import Home from "./screens/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
